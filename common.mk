2f5e311245a094396b22c5af7e9dbc40bb7a9b9a
pjgowtham <pjgowtham@gmail.com>
2023-10-08 18:17:45 +0530

    sm8450-common: Switch to inline kernel builds and modules
    Change-Id: Idaa9fe5c31b5cbcb83d98e31d493acad8f58706f

--- common.mk
+++ common.mk
@@ -396,7 +396,9 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/oplus
+    hardware/oplus \
+    kernel/oneplus/sm8450 \
+    kernel/oneplus/sm8450-modules
 
 # Storage
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
