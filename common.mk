ad9b27e312973a591560962a8ca08e3876fa5a7d
pjgowtham <pjgowtham@gmail.com>
2023-12-20 16:36:02 +0530

    sm8450-common: Update QCOM scripts from LA.VENDOR.1.0.r1-21900-WAIPIO.0
    Change-Id: I60909f66afb40b4c46d769d1ac90b76ed4aa2419

--- common.mk
+++ common.mk
@@ -227,8 +227,8 @@ PRODUCT_PACKAGES += \
 
 # Init
 PRODUCT_PACKAGES += \
-    fstab.default \
-    fstab.default.vendor_ramdisk \
+    fstab.qcom \
+    fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
