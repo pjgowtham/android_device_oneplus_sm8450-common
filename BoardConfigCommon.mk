b290b088861a4de8bb6a813b02ca50f171411873
LuK1337 <priv.luk@gmail.com>
2024-08-11 09:07:53 +0530

    sm8450-common: Include vendor/debugfs.config
    This disables the following kernel configs:
    * CONFIG_DEBUG_FS
    * CONFIG_PAGE_OWNER
    
    Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -101,7 +101,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_USES_QCOM_MERGE_DTBS_SCRIPT := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8450
-TARGET_KERNEL_CONFIG := gki_defconfig vendor/waipio_GKI.config vendor/oplus_GKI.config
+TARGET_KERNEL_CONFIG := gki_defconfig vendor/waipio_GKI.config vendor/oplus_GKI.config vendor/debugfs.config
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(TARGET_KERNEL_SOURCE)/modules.vendor_blocklist.msm.waipio
