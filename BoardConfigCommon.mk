4449c0b3780f2c8cab02e58fe61fc27f2ea7e4de
LuK1337 <priv.luk@gmail.com>
2024-08-27 00:00:55 +0200

    sm8450-common: Switch to common device_framework_matrix.xml
    Change-Id: Idc99a59a377bb3284634a62c0a1ae545bcd4e1e2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -80,7 +80,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
+    hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/framework_manifest.xml
