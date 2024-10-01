eef4fb5960d12e1cba24f37c3b5116dc100fac0f
SagarMakhar <sagarmakhar@gmail.com>
2024-10-01 23:59:30 +0530

    sm8450-common: Turn off persist.vendor.radio.poweron_opt
    It's set to 1 in qcril_database migrations. Sed the property
    to something else so it defaults to 0.
    
    When the value is 1 it breaks SMS receiving without QTI telephony.
    
    Change-Id: I7f56ecaac51abb6525c6022d92eac1c285ab0688

--- extract-files.py
+++ extract-files.py
@@ -89,6 +89,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so', 'android.hardware.security.secureclock-V1-ndk.so')
         .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
         .add_needed('android.hardware.security.rkp-V1-ndk.so'),
+    ('vendor/lib64/libqcrilNr.so', 'vendor/lib64/libril-db.so'): blob_fixup()
+        .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
 }  # fmt: skip
