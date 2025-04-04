5350bae2c3dfdbc285cd452beabb23c3c46e3066
pjgowtham <pjgowtham@gmail.com>
2025-04-04 08:18:10 +0530

    sm8450-common: Build necessary shim for oplus performance hal service
    We are unable to nuke oplus performance because it is needed by
    camera provider and c2audio.
    
    Change-Id: I7a46f8919bbcbf616e9d83d0421b445e90c17cf6

--- extract-files.py
+++ extract-files.py
@@ -58,6 +58,9 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'odm/bin/hw/vendor-oplus-hardware-performance-V1-service': blob_fixup()
+        .add_needed('libbase_shim.so')
+        .add_needed('libprocessgroup_shim.so'),
     ('odm/lib64/mediadrm/libwvdrmengine.so', 'odm/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'product/app/PowerOffAlarm/PowerOffAlarm.apk': blob_fixup()
