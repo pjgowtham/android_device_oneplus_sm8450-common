ee582d0f622cf34dfd60363f45a5eb983ed18b9a
pjgowtham <pjgowtham@gmail.com>
2025-09-17 17:41:15 +0530

    sm8450-common: Guard composer for prebuilts
    Change-Id: I0092106e02ab972fe1d4c3952a1647a8b0b0516b

--- common.mk
+++ common.mk
@@ -114,8 +114,16 @@ PRODUCT_PACKAGES += \
     init.qti.display_boot.rc \
     init.qti.display_boot.sh \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.demura-service
+
+ifeq ($(TARGET_USES_PREBUILT_HWC),true)
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.display.composer-service.rc \
+    vendor.qti.hardware.display.composer-service.xml
+else
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.display.composer-service
+endif
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
@@ -139,6 +147,7 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 
+# GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
