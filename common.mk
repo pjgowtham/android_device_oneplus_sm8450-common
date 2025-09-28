572cd5f4b3816b299efe39de7cb21d222151eb49
pjgowtham <pjgowtham@gmail.com>
2025-09-28 17:14:37 +0530

    sm8450-common: Stage post_boot scripts for cape and taro
    Change-Id: I93d092e400126337c2ce0c3fe89fce9a08ee1336

--- common.mk
+++ common.mk
@@ -179,6 +179,8 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.vendor_ramdisk \
     init.class_main.sh \
+    init.kernel.post_boot-cape.sh \
+    init.kernel.post_boot-taro.sh \
     init.oplus.hw.rc \
     init.oplus.hw.rc.recovery \
     init.oplus.rc \
