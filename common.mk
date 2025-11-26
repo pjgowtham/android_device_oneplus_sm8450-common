8c00c54d7c8f9edd9e907f7e12c623782a0d9637
asmodeus-oos <mohamed20020093@gmail.com>
2025-11-26 15:39:21 +0000

    Init improvements

--- common.mk
+++ common.mk
@@ -10,6 +10,7 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.oneplus_sm8450)
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
